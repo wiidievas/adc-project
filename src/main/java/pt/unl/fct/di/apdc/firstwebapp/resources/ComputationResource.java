d00539735ebf3027864e2631bc43e376d45e3f89
Joao Leitao <jleitao@10-139-24-107.unassigned.div-i.fct.unl.pt>
2023-02-28 23:35:49 +0000

    minor fixes

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
@@ -3,19 +3,14 @@ package pt.unl.fct.di.apdc.firstwebapp.resources;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.google.appengine.api.taskqueue.Queue;
-import com.google.appengine.api.taskqueue.QueueFactory;
-import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.gson.Gson;
 
 
@@ -44,27 +39,5 @@ public class ComputationResource {
 
 		LOG.fine("Replying to date request.");
 		return Response.ok().entity(g.toJson(fmt.format(new Date()))).build();
-	}
-
-	@GET
-	@Path("/compute")
-	public Response triggerExecuteComputeTask() {
-		Queue queue = QueueFactory.getDefaultQueue();
-		queue.add(TaskOptions.Builder.withUrl("/rest/utils/compute"));
-		return Response.ok().build();
-	}
-
-	@POST
-	@Path("/compute")
-	public Response executeComputeTask() {
-		LOG.fine("Starting to execute computation taks");
-		try {
-			Thread.sleep(1000 * 60 * 5);
-		} catch (Exception e) {
-			LOG.logp(Level.SEVERE, this.getClass().getCanonicalName(), "executeComputeTask", "An exception has ocurred", e);
-			return Response.serverError().build();
-		} //Simulates 5m execution
-		
-		return Response.ok().build();
 	}
 }
